abe0419f8fd7048a0c39b2f478f9a6e0c2a5a6c9
Chris Denman <chrisdenman@ceilingcat.co.uk>
2023-03-11 15:40:05 +0000

    Comments

--- src/misc/ArrayHelpers.js
+++ src/misc/ArrayHelpers.js
@@ -1,14 +1,20 @@
+/**
+ * Miscellaneous array helper functions.
+ */
 export default class ArrayHelpers {
 
     /**
-     * @param {*[]} array
-     * @param {function(*): boolean} [predicate]
-     * @return {*}
+     * Returns the first element of an array that satisfied a predicate if supplied, else the first element of an array.
+     *
+     * @param {*[]} array - an array of any type that the given predicate accepts
+     * @param {function(*): boolean} [predicate=undefined] - an optional predicate
+     * @return {*} the first element of 'array' that satisfies 'predicate', else the first element of the array
      */
-    static first = (array, predicate) => predicate === undefined ? array[0] : array.filter(predicate)[0];
+    static first = (array, predicate = undefined) =>
+        predicate === undefined ? array[0] : array.filter(predicate)[0];
 
     /**
-     * Returns a copy of 'array' but with all <code>undefined</code> values removed.
+     * Returns a copy of 'array' with all <code>undefined</code> values removed.
      *
      * @param {*[]} array the array to filter
      *
